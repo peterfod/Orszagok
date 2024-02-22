4880f586d209268fe0ab2aef7c5e247a89447fe8
fodor.petya <fodor.petya@M019-T1>
2024-02-22 11:54:54 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Orszagok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnAtlag = new System.Windows.Forms.Button();
+            this.lblEredmeny = new System.Windows.Forms.Label();
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuMegnyit = new System.Windows.Forms.ToolStripMenuItem();
+            this.megnyitásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKilepes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEuropaiak = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(109, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Országok népessége";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 94);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 329);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(310, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mit számoljak";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(34, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Legfeljebb 10 millió";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(34, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(100, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "10 milliónál több";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnAtlag
+            // 
+            this.btnAtlag.Location = new System.Drawing.Point(314, 400);
+            this.btnAtlag.Name = "btnAtlag";
+            this.btnAtlag.Size = new System.Drawing.Size(104, 23);
+            this.btnAtlag.TabIndex = 3;
+            this.btnAtlag.Text = "Átlag népesség";
+            this.btnAtlag.UseVisualStyleBackColor = true;
+            // 
+            // lblEredmeny
+            // 
+            this.lblEredmeny.AutoSize = true;
+            this.lblEredmeny.Location = new System.Drawing.Point(311, 351);
+            this.lblEredmeny.Name = "lblEredmeny";
+            this.lblEredmeny.Size = new System.Drawing.Size(54, 13);
+            this.lblEredmeny.TabIndex = 4;
+            this.lblEredmeny.Text = "Eredmény";
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(424, 400);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(75, 23);
+            this.btnKilepes.TabIndex = 5;
+            this.btnKilepes.Text = "Kilépés";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.menuKilepes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Legnagyobb/legkisebb népesség";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Legnagyobb",
+            "Legkisebb"});
+            this.comboBox1.Location = new System.Drawing.Point(314, 299);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuMegnyit,
+            this.menuEuropaiak});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(548, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuMegnyit
+            // 
+            this.menuMegnyit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.megnyitásToolStripMenuItem,
+            this.menuKilepes});
+            this.menuMegnyit.Name = "menuMegnyit";
+            this.menuMegnyit.Size = new System.Drawing.Size(37, 20);
+            this.menuMegnyit.Text = "Fájl";
+            // 
+            // megnyitásToolStripMenuItem
+            // 
+            this.megnyitásToolStripMenuItem.Name = "megnyitásToolStripMenuItem";
+            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.megnyitásToolStripMenuItem.Text = "Megnyitás...";
+            // 
+            // menuKilepes
+            // 
+            this.menuKilepes.Name = "menuKilepes";
+            this.menuKilepes.Size = new System.Drawing.Size(138, 22);
+            this.menuKilepes.Text = "Kilépés";
+            this.menuKilepes.Click += new System.EventHandler(this.menuKilepes_Click);
+            // 
+            // menuEuropaiak
+            // 
+            this.menuEuropaiak.Name = "menuEuropaiak";
+            this.menuEuropaiak.Size = new System.Drawing.Size(71, 20);
+            this.menuEuropaiak.Text = "Európaiak";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 464);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKilepes);
+            this.Controls.Add(this.lblEredmeny);
+            this.Controls.Add(this.btnAtlag);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Országok népessége";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnAtlag;
+        private System.Windows.Forms.Label lblEredmeny;
+        private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuMegnyit;
+        private System.Windows.Forms.ToolStripMenuItem megnyitásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuKilepes;
+        private System.Windows.Forms.ToolStripMenuItem menuEuropaiak;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orszagok
+{
+    public partial class Form1 : Form
+    {
+        public static List<Orszag> lista = new List<Orszag>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //var sr = new StreamReader("orszagok.txt", Encoding.GetEncoding("iso-8859-1"));
+            var sr = new StreamReader("orszagok.txt", Encoding.Default);
+            string[] haromSor = new string[3];
+            while (!sr.EndOfStream)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    haromSor[i] = sr.ReadLine();
+                }
+                var egyOrszagAdatai = new Orszag(haromSor);
+                lista.Add(egyOrszagAdatai);
+            }
+            sr.Close();
+
+            foreach (var sor in lista)
+            {
+                richTextBox1.Text += sor.Nev + "\n";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            int nepesseg10mfolott = (from n in lista
+                                     where n.Nepesseg > 10000
+                                     select n).Count();
+            textBox1.Text = nepesseg10mfolott.ToString();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            int nepesseg10malatt = (from n in lista
+                                    where n.Nepesseg <= 10000
+                                    select n).Count();
+            textBox1.Text = nepesseg10malatt.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var legkisebbnagyobb = from sor in lista
+                                   orderby sor.Nepesseg
+                                   select sor;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    lblEredmeny.Text = legkisebbnagyobb.Last().Nev + " ";
+                    lblEredmeny.Text += legkisebbnagyobb.Last().Nepesseg.ToString() + " eFő";
+                    break;
+                case 1:
+                    lblEredmeny.Text = legkisebbnagyobb.First().Nev + " ";
+                    lblEredmeny.Text += legkisebbnagyobb.First().Nepesseg.ToString() + " eFő";
+                    break;
+            }
+        }
+    }
+
+    public class Orszag
+    {
+        public string Nev { get; set; }
+        public int Nepesseg { get; set; }
+        public string Kontinens { get; set; }
+
+        public Orszag(string[] sorok)
+        {
+            Nev = sorok[0];
+            Nepesseg = Convert.ToInt32(sorok[1]);
+            Kontinens = sorok[2];
+        }
+    }
+}
